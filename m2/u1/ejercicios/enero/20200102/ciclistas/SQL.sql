8e1ccb5fc82b74a226a5288e6d294e86a4b37329
anferasu <andresfeliperamos@hotmail.com>
2020-01-02 11:12:56 +0100

    sql 20200102

---
+++ m2/u1/ejercicios/enero/20200102/ciclistas/SQL.sql
@@ -0,0 +1,32 @@
+﻿--1.1 Listar las edades de los ciclistas (sin repetidos)
+SELECT
+  DISTINCT edad
+FROM public.ciclista ORDER BY edad ASC
+
+--1.2 Listar las edades de los ciclistas de Artiach
+  SELECT
+   DISTINCT edad
+FROM public.ciclista WHERE nomequipo = 'Artiach' ORDER BY edad ASC
+
+--1.3 Listar las edades de los ciclistas de Artiach o de Amore Vita
+SELECT
+   DISTINCT edad
+FROM public.ciclista WHERE nomequipo = 'Artiach' or nomequipo = 'Amore Vita' ORDER BY edad ASC
+
+--1.4 listar los dorsales de los ciclistas cuya edad sea menor que 25 o mayor que 30
+SELECT
+  dorsal
+FROM public.ciclista WHERE edad < 25 OR edad >30
+
+--1.5
+SELECT
+  dorsal
+FROM public.ciclista WHERE edad BETWEEN 28 AND 32 AND nomequipo='Banesto' 
+
+--1.6
+
+SELECT 
+SUBSTR(nombre, 1, STRPOS(nombre, ' ')) AS nombre_ciclista
+FROM public.ciclista WHERE LENGTH(nombre) > 8
+
+
---
+++ m2/u1/ejercicios/enero/20200102/ciclistas/ciclistas.sql
@@ -0,0 +1,99 @@
+﻿-- ----------------------------------------------------------------------
+-- MySQL Migration Toolkit
+-- SQL Create Script
+-- ----------------------------------------------------------------------
+
+--SET FOREIGN_KEY_CHECKS = 0;
+
+--CREATE DATABASE IF NOT EXISTS ciclistas
+  --CHARACTER (SET latin1 COLLATE latin1_swedish_ci);
+
+
+DROP DATABASE ciclistas;
+CREATE DATABASE Ciclistas;
+  --CHARACTER (SET latin1 COLLATE latin1_swedish_ci);
+
+--USE ciclistas;
+-- -------------------------------------
+-- Tables
+
+--DROP TABLE IF EXISTS ciclistas.ciclista;
+CREATE TABLE ciclista (
+  dorsal INTEGER NOT NULL,
+  nombre VARCHAR(30) NOT NULL,
+  edad INTEGER NULL,
+  nomequipo VARCHAR(25) NOT NULL,
+  PRIMARY KEY (dorsal)
+ -- INDEX equipociclista (nomequipo)
+);
+--ENGINE = INNODB
+--CHARACTER (SET utf8 COLLATE utf8_general_ci);
+
+--DROP TABLE IF EXISTS ciclistas.equipo;
+CREATE TABLE equipo (
+  nomequipo VARCHAR(25) NOT NULL,
+  director VARCHAR(30) NULL,
+  PRIMARY KEY (nomequipo)
+);
+--ENGINE = INNODB
+--CHARACTER (SET utf8 COLLATE utf8_general_ci);
+
+--DROP TABLE IF EXISTS ciclistas.etapa;
+CREATE TABLE etapa (
+  numetapa INTEGER NOT NULL,
+  kms INTEGER NOT NULL,
+  salida VARCHAR(35) NOT NULL,
+  llegada VARCHAR(35) NOT NULL,
+  dorsal INTEGER NULL,
+  PRIMARY KEY (numetapa)
+  --INDEX ciclistaetapa (dorsal)
+);
+--ENGINE = INNODB
+--CHARACTER (SET utf8 COLLATE utf8_general_ci);
+
+--DROP TABLE IF EXISTS ciclistas.lleva;
+CREATE TABLE lleva (
+  dorsal INTEGER NOT NULL,
+  numetapa INTEGER NOT NULL,
+  código VARCHAR(3) NOT NULL,
+  PRIMARY KEY (numetapa, código)
+  --INDEX ciclistallevar (dorsal),
+  --INDEX etapallevar (numetapa),
+  --INDEX maillotllevar (código)
+);
+--ENGINE = INNODB
+--CHARACTER (SET utf8 COLLATE utf8_general_ci);
+
+--DROP TABLE IF EXISTS ciclistas.maillot;
+CREATE TABLE maillot (
+  código VARCHAR(3) NOT NULL,
+  tipo VARCHAR(30) NOT NULL,
+  color VARCHAR(20) NOT NULL,
+  premio INTEGER NOT NULL,
+  PRIMARY KEY (código)
+);
+--ENGINE = INNODB
+--CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+--DROP TABLE IF EXISTS ciclistas.puerto;
+CREATE TABLE puerto (
+  nompuerto VARCHAR(35) NOT NULL,
+  altura INTEGER NOT NULL,
+  categoria VARCHAR(1) NOT NULL,
+  pendiente DOUBLE PRECISION NULL,
+  numetapa INTEGER NOT NULL,
+  dorsal INTEGER NULL,
+  PRIMARY KEY (nompuerto)
+  --INDEX ciclistapuerto (dorsal),
+  --INDEX etapapuerto (numetapa)
+);
+--ENGINE = INNODB
+--CHARACTER (SET utf8 COLLATE utf8_general_ci);
+
+
+
+--SET FOREIGN_KEY_CHECKS = 1;
+
+-- ----------------------------------------------------------------------
+-- EOF
+
