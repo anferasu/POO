29de497bdc4a8e855bb4b765361e65a4facb4e2b
anferasu <andresfeliperamos@hotmail.com>
2020-01-20 14:04:36 +0100

    sql 20200120

--- m2/u1/ejercicios/enero/20200116/SQL_libro.sql
+++ m2/u1/ejercicios/enero/20200116/SQL_libro.sql
@@ -1,14 +1,14 @@
 ﻿--1
-WITH libro
-(SELECT
+SELECT
    a.nombre_al, a.edad_al
 FROM public.alumnos a
 INNER JOIN public.estudian e
   ON a.dni_al = e.dni_al
 INNER JOIN public.asignaturas a1
   ON e.cod_as = a1.cod_as
-WHERE a1.nombre_as like 'D%';)
+WHERE a1.nombre_as like 'Se%';
 
+select a.nombre_al, a.edad_al from alumnos a join estudian e using (dni_al) join asignaturas al  on  e.cod_as = al.cod_as where al.nombre_as ='Secretary';
 /* 
 Trainer/Consultant                                
 Sales Representative                              
@@ -33,7 +33,7 @@ WHERE a.edad_al BETWEEN 18 AND 25;
 --4
 SELECT p.nombre_p, p.telefono_p from
 public.profesores p
-WHERE p.nombre_p LIKE 'P%';
+WHERE p.apellido1_p LIKE 'M%';
 
 --5
 SELECT a.nombre_al, a.apellido1_al FROM
@@ -44,7 +44,7 @@ INNER JOIN public.asignaturas a1
 ON a1.cod_as = e.cod_as
 INNER JOIN public.profesores p
 ON p.dni_p = a1.dni_p
-WHERE p.nombre_p like 'Pepe';
+WHERE p.nombre_p like 'Raba%';
 
 --6
 
@@ -60,6 +60,20 @@ WHERE e.nota_al_as IS NOT NULL AND e.nota_al_as IN (SELECT MAX(e.nota_al_as) FRO
 SELECT * FROM estudian e WHERE e.nota_al_as >= 9;
 
 
+select * from alumnos a, estudian e where a.dni_al = e.dni_al and e.nota_al_as >= all (select e.nota_al_as from estudian e);
+
+SELECT a.nombre_al, a.apellido1_al, a1.nombre_as, e.nota_al_as FROM
+public.alumnos a
+INNER JOIN public.estudian e
+ON a.dni_al =e.dni_al
+INNER JOIN public.asignaturas a1
+ON a1.cod_as = e.cod_as
+join(select e.cod_as, max(e.nota_al_as) from estudian e group by e.cod_as) as nota_max
+on e.cod_as = nota_max.cod_as
+and e.nota_al_as = nota_max.max;
+
+
+
 --7
 
 SELECT (a.nombre_al ||' '|| a.apellido1_al ||' '||a.apellido2_al) AS nombre_completo, a.direccion_al 
@@ -73,6 +87,8 @@ ORDER BY a.edad_al DESC;
 
 --9
 
+select a.dni_al, a.nombre_al from alumnos a
+where a.dni_al in (select e.dni_al from alumnos a join estudian e using (dni_al) group by a.dni_al having count (a.dni_al) >= 2)
 
 --10
 SELECT * FROM profesores p WHERE dni_p = '82341356H';
@@ -88,7 +104,20 @@ WHERE apellido1_p = 'Martínez';
 
 --12
 
+SELECT a.nombre_al, e.nota_al_as, a1.nombre_as FROM alumnos a JOIN estudian e
+  ON a.dni_al = e.dni_al JOIN asignaturas a1 ON e.cod_as = a1.cod_as
+  JOIN (select e.dni_al,COUNT(e.dni_al) from estudian e 
+  group by e.dni_al having count (e.dni_al) >= 2) masdedos
+  ON a.dni_al = masdedos.dni_al;
+
+
 --13
+
+  SELECT a1.nombre_as, p.nombre_p, AVG(e.nota_al_as) AS Media, MIN(e.nota_al_as) AS Minimo, MAX(e.nota_al_as) AS maximo
+    FROM profesores p INNER JOIN asignaturas a1 ON p.dni_p = a1.dni_p
+    INNER JOIN estudian e USING (cod_as)
+    GROUP BY a1.cod_as, p.nombre_p;
+
 
 --14
 SELECT * FROM alumnos a ORDER BY a.telefono_al ASC;
@@ -101,15 +130,18 @@ ORDER BY a.edad_al DESC, nombre_al ASC;
 SELECT (a.nombre_al ||' '|| a.apellido1_al ||' '||a.apellido2_al) AS nombre_completo, e.nota_al_as FROM alumnos a
 INNER JOIN estudian e
 ON a.dni_al = e.dni_al
-WHERE e.nota_al_as BETWEEN 7 AND 9 OR a.ciudad_al = 'Mandurah';
+WHERE e.nota_al_as NOT BETWEEN 7 AND 9 OR a.ciudad_al = 'Merang';
+
+
+SELECT (a.nombre_al ||' '|| a.apellido1_al ||' '||a.apellido2_al) AS nombre_completo, e.nota_al_as FROM alumnos a, estudian e
+WHERE a.dni_al = e.dni_al 
+AND (e.nota_al_as NOT BETWEEN 7 AND 9 OR a.ciudad_al = 'Merang') AND e.nota_al_as IS NOT NULL;
 
 --17
-SELECT (a.nombre_al ||' '|| a.apellido1_al ||' '||a.apellido2_al) AS nombre_completo, a1.nombre_as FROM alumnos a
+SELECT (a.nombre_al ||' '|| a.apellido1_al ||' '||a.apellido2_al) AS nombre_completo FROM alumnos a
 INNER JOIN estudian e
-ON a.dni_al = e.dni_al
-INNER JOIN asignaturas a1
-ON e.cod_as = a1.cod_as
-WHERE e.nota_al_as > 5;
+USING (dni_al)
+WHERE e.nota_al_as >= 5;
 
 --18
 SELECT  * FROM alumnos a WHERE a.nombre_al LIKE 'Jhon%';
@@ -117,8 +149,16 @@ SELECT  * FROM alumnos a WHERE a.nombre_al LIKE 'Jhon%';
 SELECT a.nombre_al, TRUNC(AVG (e.nota_al_as),2) FROM estudian e
 INNER JOIN alumnos a
 ON e.dni_al = a.dni_al
-WHERE a.nombre_al LIKE 'F%'
+WHERE a.nombre_al LIKE 'Alfons%'
 group BY a.nombre_al;
+
+--19
+
+SELECT a.nombre_as,ROUND(AVG(e.nota_al_as),2) AS media, MAX(e.nota_al_as) AS maximo, MIN (e.nota_al_as) AS minimo, MAX(e.nota_al_as)- MIN (e.nota_al_as) AS diferencia, 
+  COUNT(*) AS numeroalumnos FROM estudian e
+  JOIN asignaturas a ON e.cod_as = a.cod_as
+  GROUP BY a.nombre_as
+  
 
 --20
 --No existe campo fecha
@@ -129,10 +169,6 @@ INNER JOIN alumnos a
 ON e.dni_al = a.dni_al
 WHERE e.nota_al_as IS NOT null
 group BY a.nombre_al;
-
-
-
-
 
 
 
