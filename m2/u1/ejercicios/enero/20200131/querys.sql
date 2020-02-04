5cabedb80f9dbbf6fc15e48c489f96d5c4d5a1ee
anferasu <andresfeliperamos@hotmail.com>
2020-02-04 14:05:48 +0100

    sql 20200204 m1u2h6

--- m2/u1/ejercicios/enero/20200131/querys.sql
+++ m2/u1/ejercicios/enero/20200131/querys.sql
@@ -1,4 +1,6 @@
-﻿create view consulta1 as
+﻿--1
+
+create view consulta1 as
 select c."cÓdigo_cliente" as "CODIGO CLIENTE", 
        p."nÚmero_de_pedido" as "NUMERO PEDIDO",
        c.empresa as "EMPRESA",
@@ -6,7 +8,9 @@ select c."cÓdigo_cliente" as "CODIGO CLIENTE",
 from clientes c
 INNER JOIN pedidos p
 ON c."cÓdigo_cliente"=p."cÓdigo_cliente"
-WHERE c."poblaciÓn" in ('MADRID', 'BARCELONA', 'ZARAGOZA')
+WHERE c."poblaciÓn" in ('MADRID', 'BARCELONA', 'ZARAGOZA');
+
+--4
 
 create view consulta1optimizada as
 SELECT c1."cÓdigo_cliente", c1.empresa, c1."poblaciÓn", p."nÚmero_de_pedido" from pedidos p
@@ -23,7 +27,67 @@ SELECT c."cÓdigo_cliente", c.empresa, c."poblaciÓn" from clientes c
 create view consulta2optimizada as
 SELECT c1."cÓdigo_cliente", c1.empresa, c1."poblaciÓn", p."nÚmero_de_pedido" from pedidos p
   JOIN(select * from subconsulta2optimizada)c1  
-  on c1."cÓdigo_cliente" = p."cÓdigo_cliente"
+  on c1."cÓdigo_cliente" = p."cÓdigo_cliente";
+
+
+--6
+create view subconsulta1consulta2 as
+--consulta 1
+select p."nÚmero_de_pedido", p.fecha_de_pedido, p.forma_de_pago, p."cÓdigo_cliente"  from pedidos p
+  where date_part('year',p.fecha_de_pedido)=2002 and p.forma_de_pago = 'TARJETA';
+
+
+--consulta completa
+SELECT c.empresa,
+       c."poblaciÓn",
+       c1."nÚmero_de_pedido",
+       c1.fecha_de_pedido,
+       c1.forma_de_pago
+  FROM clientes c
+JOIN (select p."nÚmero_de_pedido", p.fecha_de_pedido, p.forma_de_pago, p."cÓdigo_cliente"  from pedidos p
+  where date_part('year',p.fecha_de_pedido)=2002 and p.forma_de_pago = 'TARJETA') c1
+using (cÓdigo_cliente)
+order by c1.fecha_de_pedido asc;
+
+--consulta ejecutando vista de la consulta 1
+SELECT c.empresa,
+       c."poblaciÓn",
+       c1."nÚmero_de_pedido",
+       c1.fecha_de_pedido,
+       c1.forma_de_pago
+  FROM clientes c
+JOIN (select * from subconsulta1consulta2 )c1  
+  on c."cÓdigo_cliente" = c1."cÓdigo_cliente"
+order by c1.fecha_de_pedido asc;
+
+
+--7
+
+--consulta 1
+
+create view c1consulta3 as
+select COUNT(*) as n_pedidos, p."cÓdigo_cliente" from pedidos p
+GROUP by p."cÓdigo_cliente";
+
+--consulta 2
+create view c2consulta3 as
+SELECT max(c1.n_pedidos) as maximo  from (select COUNT(*) as n_pedidos, p."cÓdigo_cliente" from pedidos p
+GROUP by p."cÓdigo_cliente")c1;
+
+--consulta 3
+SELECT c1."cÓdigo_cliente"  from 
+(select COUNT(*) as n_pedidos, p."cÓdigo_cliente" from pedidos p
+GROUP by p."cÓdigo_cliente")c1
+join 
+(SELECT max(c1.n_pedidos) as maximo  from (select COUNT(*) as n_pedidos, p."cÓdigo_cliente" from pedidos p
+GROUP by p."cÓdigo_cliente")c1)c2
+on c1.n_pedidos=c2.maximo
+
+SELECT c1."cÓdigo_cliente"  from 
+(select * from c1consulta3)c1
+join 
+(select * from c2consulta3)c2
+on c1.n_pedidos=c2.maximo
 
 
 select * from clientes; 
---
+++ m2/u1/ejercicios/febrero/20200204/querys.sql
@@ -0,0 +1,15 @@
+﻿--2
+create view consultaproductos as
+select p."cÓdigo_artÍculo", p."secciÓn", p.precio from productos p where p.precio >100;
+
+select c."cÓdigo_artÍculo", c."secciÓn", c.precio from consultaproductos c
+where (c.secciÓn ='DEPORTES');
+
+select c."cÓdigo_artÍculo", c."secciÓn", c.precio from consultaproductos c
+where (c.secciÓn ='DEPORTES');
+
+INSERT INTO productos ('AR90','NOVEDADES',5) 
+select * from consultaproductos
+
+
+
