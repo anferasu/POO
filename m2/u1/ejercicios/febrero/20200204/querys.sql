ef2be117b2164f0a2d92e9ec23dc68fcbc27555a
anferasu <andresfeliperamos@hotmail.com>
2020-02-05 14:05:09 +0100

    20200205 sql

--- m2/u1/ejercicios/febrero/20200204/querys.sql
+++ m2/u1/ejercicios/febrero/20200204/querys.sql
@@ -2,14 +2,32 @@
 create view consultaproductos as
 select p."cÓdigo_artÍculo", p."secciÓn", p.precio from productos p where p.precio >100;
 
+create view consultacompletaproductos as
 select c."cÓdigo_artÍculo", c."secciÓn", c.precio from consultaproductos c
 where (c.secciÓn ='DEPORTES');
 
-select c."cÓdigo_artÍculo", c."secciÓn", c.precio from consultaproductos c
-where (c.secciÓn ='DEPORTES');
 
-INSERT INTO productos ('AR90','NOVEDADES',5) 
-select * from consultaproductos
+ALTER table productos alter importado set default 0;
+
+INSERT INTO consultaproductos VALUES('AR90','NOVEDADES',5); 
+
+--6
+-- se activa la opcion check local
+INSERT INTO consultaproductos VALUES('AR89','NOVEDADES',5); 
+--1 el nuevo registro para la vista «consultaproductos» viola la opción check porque no cumple con la condicion mayor a 100
+INSERT INTO consultaproductos VALUES('AR89','NOVEDADES',110);
+--si deja guardar porque el valor precio si es mayor que 100 en la columna
+
+--8
+
+INSERT INTO consultacompletaproductos VALUES('AR97','DEPORTES',5);  
+
+select * from consultacompletaproductos
+
+    select * from consultaproductos
+
+    select * from productos   
+
 
 
 
